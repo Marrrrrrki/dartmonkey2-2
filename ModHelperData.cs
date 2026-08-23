c269e53f6895fc24912ec6ea462c589cb371bc10
Marrrrrrki <f_mcguirk@icloud.com>
2026-08-23 18:49:41 +1000

    Update mod description for DartMonkey2

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,8 +6,8 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "DartMonkey2";
 
-    public const string Description = "An empty mod";
+    public const string Description = "Adds a second Dart Monkey to BTD6.";
 
-    public const string RepoOwner = "Marrrrrrki"; // TODO add your GitHub username here, also in the download URL in README.md
-    public const string RepoName = "dartmonkey2-2"; // TODO: add your repo name here, also in the download URL in README.md
+    public const string RepoOwner = "Marrrrrrki";
+    public const string RepoName = "dartmonkey2-2";
 }
