370dff3292b1cdfc9e4662cfd3c76b7d11155e60
Marrrrrrki <f_mcguirk@icloud.com>
2026-08-23 18:46:34 +1000

    Create ModHelperData.cs for mod configuration
    This file contains static data for the DartMonkey2 mod, including versioning and repository information.

---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+namespace DartMonkey2;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "55.0";
+    public const string Version = "1.0.0";
+    public const string Name = "DartMonkey2";
+
+    public const string Description = "An empty mod";
+
+    public const string RepoOwner = "Marrrrrrki"; // TODO add your GitHub username here, also in the download URL in README.md
+    public const string RepoName = "dartmonkey2-2"; // TODO: add your repo name here, also in the download URL in README.md
+}
