c049451d8f42a15623cc7fa7713d8c69d71ddb39
Marrrrrrki <f_mcguirk@icloud.com>
2026-08-23 18:50:11 +1000

    Update WorksOnVersion to 56.0

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,7 +2,7 @@ namespace DartMonkey2;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "55.0";
+    public const string WorksOnVersion = "56.0";
     public const string Version = "1.0.0";
     public const string Name = "DartMonkey2";
 
