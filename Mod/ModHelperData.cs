eaca318f9fb53c3d8949fe7f0076383c7181f80d
Marrrrrrki <f_mcguirk@icloud.com>
2026-08-29 13:03:45 +1000

    Add ModHelperData class for DartMonkey2 mod

---
+++ Mod/ModHelperData.cs
@@ -0,0 +1,13 @@
+namespace DartMonkey2;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "56.0";
+    public const string Version = "1.0.0";
+    public const string Name = "DartMonkey2";
+
+    public const string Description = "Adds a second Dart Monkey to BTD6.";
+
+    public const string RepoOwner = "Marrrrrrki";
+    public const string RepoName = "dartmonkey2-2";
+}
